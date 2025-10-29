c432a17c62beaf582fe1e5f8c891966d366a45e8
ForestOfLight <briancassriel@gmail.com>
2025-10-28 22:13:42 -0700

    bump versions

--- scripts/config.js
+++ scripts/config.js
@@ -4,7 +4,7 @@ const extension = new CanopyExtension({
     author: 'ForestOfLight',
     name: 'Understudy',
     description: 'Simulated Players for §l§aCanopy§r',
-    version: '1.0.13'
+    version: '1.1.0'
 });
 
 export default extension;
