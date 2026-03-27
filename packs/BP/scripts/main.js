1caeb889074e1597df52a20dc1eb5d9baa17689d
ForestOfLight <briancassriel@gmail.com>
2026-03-27 14:19:04 -0700

    bump pack and mc version
    - Pack: 1.2.1
    - MC: 1.26.10

--- packs/BP/scripts/main.js
+++ packs/BP/scripts/main.js
@@ -4,7 +4,7 @@ export const extension = new CanopyExtension({
     author: 'ForestOfLight',
     name: 'Understudy',
     description: 'Simulated Players for §l§aCanopy§r',
-    version: '1.2.0'
+    version: '1.2.1'
 });
 
 import './commands/join';
